44b713f7d287017d7db9c57c56ce4fc6797fdb90
Michael Chary <mac389@gmail.com>
2014-11-09 16:22:42 -0500

    update

--- simpler-synfire.py
+++ simpler-synfire.py
@@ -1,21 +1,29 @@
 import numpy as np
+import utils as tech
 import matplotlib.pyplot as plt 
 
-n = 10
+
+'''
+  #Different distributions of gap junctions (masked arrays)
+
+'''
+n = 15
 weights = np.eye(n,k=-1)
 INITIAL = 0
 
-timesteps = 5
+timesteps = 150
+conditions = 3
+timesteps *= conditions
 v = np.zeros((n,timesteps))
-spiketimes = []
+spiketimes = {neuron:[] for neuron in xrange(n)}
 
 El = -60 #mV
 Ee = 0 #mV
 Ei = -10 #mV
 Vt = -40 #mV
 epsilon = 0.01 #ratio of timestep to membrane time constant
-g_gap = 1 #nS #equivalent coupling
-g_e = .4 #nS 
+g_gap = 40 #nS #equivalent coupling
+g_e = 20 #nS 
 
 #--initial conditions
 v[:,INITIAL] = El +(Vt-El)*np.random.rand(n)
@@ -24,7 +32,16 @@ verbose = True
 for t in xrange(1,timesteps):
 
 	#first effect of gap junctions
-#	v[:,t] += g_gap * (np.roll(v[:,t],-1)-v[:,t]) + g_gap * (np.roll(v[:,t],-1)-v[:,t])
+	left =  np.roll(v[:,t-1],-1)
+	left[0]=v[:,t-1][0]
+
+	right  = np.roll(v[:,t-1],1)
+	right[-1] = v[:,t-1][-1]
+	if verbose:
+		print '----FX of gap junctions-----------'
+		print g_gap * (left-v[:,t-1]) + g_gap * (right-v[:,t-1])
+		print '----FX of gap junctions-----------'
+ 	v[:,t] += g_gap * (left-v[:,t-1]) + g_gap * (right-v[:,t-1])
 
 	if verbose:
 		print '----Starting voltage------------'
@@ -34,7 +51,7 @@ for t in xrange(1,timesteps):
 		print '----Leak change-----------------'
 		print epsilon*(-v[:,t-1])
 		print '--------------------------------'
-	v[:,t] = v[:,t-1] + epsilon*(-v[:,t-1])
+	v[:,t] += (v[:,t-1] + epsilon*(-v[:,t-1]))
 
 	#then synaptic effect
 	idx = v[:,t] > Vt
@@ -46,7 +63,10 @@ for t in xrange(1,timesteps):
 		print '--------------------------------'
 
 	#Record spiketimes
-	spiketimes.extend((t,np.nonzero(idx)[0]))
+	for neuron in np.nonzero(idx)[0]:
+		times = spiketimes[neuron]
+		times.append(t)
+		spiketimes[neuron] = times
 	#Propagate effect
 	v[:,t][np.nonzero(idx)[0]] = El
 	if verbose:
@@ -54,16 +74,34 @@ for t in xrange(1,timesteps):
 		print v[:,t]
 		print '--------------------------------'
 
-	for neuron in idx: #Propagate effect
+	for neuron in np.nonzero(idx)[0]: #Propagate effect
 		v[:,t] += g_e*weights[:,neuron]
 
-print spiketimes
 
 #--- raster
 fig = plt.figure()
 ax = fig.add_subplot(111)
-for ith, (time,neurons) in enumerate(spiketimes):
-	if len(neurons) > 0:
-		ax.vlines(neurons, ith + .5, ith + 1.5, color='k')
-ax.ylim(.5, len(event_times_list) + .5)
+for neuron in xrange(n):
+	if neuron in spiketimes and len(spiketimes[neuron]) > 0:
+		ax.vlines(spiketimes[neuron],  neuron+ .5, neuron + 1.5, color='k')
+ax.set_ylim(-1, len(spiketimes))
+tech.adjust_spines(ax)
+ax.yaxis.grid()
+ax.set_yticks(xrange(n))
+ax.set_xlim(xmin=0,xmax=timesteps)
+ax.set_xlabel('Time')
+ax.set_ylabel('Neurons')
+fig.tight_layout()
+'''
+#--- voltage
+trace,panels = plt.subplots(nrows=n,ncols=1,sharex=True)
+for neuron,panel in enumerate(panels):
+	panel.plot(v[neuron,:])
+	tech.adjust_spines(panel,['left'] if neuron < (n-1) else ['left','bottom'])
+	panel.set_ylim(ymax=v.max(),ymin=v.min())
+	panel.set_yticks([np.floor(v.min()),np.ceil(v.max())])
+	panel.set_xticks(range(timesteps))
+trace.tight_layout()
+trace.subplots_adjust(hspace=0.5)
+'''
 plt.show()
